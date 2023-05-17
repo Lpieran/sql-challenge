889efa76328aa7903a03315a8b7de09327625b1c
Lpieran <127270151+Lpieran@users.noreply.github.com>
2023-05-16 21:35:25 -0700

    updated

--- employee_query.sql
+++ employee_query.sql
@@ -1,18 +1,18 @@
--- List the employee number, last name, first name, sex, and salary of each employee
+-- List the employee number, last name, first name, sex, and salary of each employee.
 SELECT e.emp_no, e.last_name, e.first_name, e.sex, s.salary
 FROM employees e
-JOIN salaries s ON e.emp_no = s.emp_no;
+INNER JOIN salaries s ON e.emp_no = s.emp_no;
 
--- List the first name, last name, and hire date for the employees who were hired in 1986
+-- List the first name, last name, and hire date for the employees who were hired in 1986.
 SELECT first_name, last_name, hire_date
 FROM employees
 WHERE hire_date BETWEEN '1986-01-01' AND '1986-12-31';
 
--- List the manager of each department along with their department number, department name, employee number, last name, and first name
-SELECT dm.emp_no, d.dept_no, d.dept_name, e.emp_no, e.last_name, e.first_name
+-- List the manager of each department along with their department number, department name, employee number, last name, and first name.
+SELECT  dm.dept_no, d.dept_name, e.emp_no, e.last_name, e.first_name
 FROM dept_manager dm
-JOIN departments d ON dm.dept_no = d.dept_no
-JOIN employees e ON dm.emp_no = e.emp_no;
+INNER JOIN departments d ON dm.dept_no = d.dept_no
+INNER JOIN employees e ON dm.emp_no = e.emp_no;
 
 -- List the department number for each employee along with that employee’s employee number, last name, first name, and department name
 SELECT d.dept_no, e.emp_no, e.last_name, e.first_name, d.dept_name
@@ -20,16 +20,16 @@ FROM employees e
 JOIN dept_emp de ON e.emp_no = de.emp_no
 JOIN departments d ON de.dept_no = d.dept_no;
 
--- List first name, last name, and sex of each employee whose first name is Hercules and whose last name begins with the letter B
+-- List first name, last name, and sex of each employee whose first name is Hercules and whose last name begins with the letter B.
 SELECT first_name, last_name, sex
 FROM employees
 WHERE first_name = 'Hercules' AND last_name LIKE 'B%';
 
--- List each employee in the Sales department, including their employee number, last name, and first name
+-- List each employee in the Sales department, including their employee number, last name, and first name.
 SELECT e.emp_no, e.last_name, e.first_name
 FROM employees e
-JOIN dept_emp de ON e.emp_no = de.emp_no
-JOIN departments d ON de.dept_no = d.dept_no
+INNER JOIN dept_emp de ON e.emp_no = de.emp_no
+INNER JOIN departments d ON de.dept_no = d.dept_no
 WHERE d.dept_name = 'Sales';
 
 -- List each employee in the Sales and Development departments, including their employee number, last name, first name, and department name
@@ -45,8 +45,8 @@ FROM employees
 GROUP BY last_name
 ORDER BY freq_count DESC;
 
-SELECT * FROM titles;
 SELECT * FROM departments;
+SELECT * FROM titles;
 SELECT * FROM employees;
 SELECT * FROM dept_emp;
 SELECT * FROM dept_manager;
--- schemata.sql
+++ schemata.sql
@@ -30,18 +30,21 @@ CREATE TABLE dept_emp (
   emp_no INT NOT NULL,
   dept_no VARCHAR(4) NOT NULL,
   FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
-  FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+  FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+  PRIMARY KEY (emp_no, dept_no)
 );
 
 CREATE TABLE dept_manager (
   dept_no VARCHAR(4) NOT NULL,
   emp_no INT NOT NULL,
   FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
-  FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+  FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+  PRIMARY KEY (dept_no, emp_no)
 );
 
 CREATE TABLE salaries (
-  "emp_no" INT   NOT NULL,
-  "salary" INT   NOT NULL
+  emp_no INT NOT NULL,
+  salary INT NOT NULL,
+  FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+  PRIMARY KEY (emp_no)
 );
-
